e4b54cb0d230ffcc4f30dc835aab0b7fcd41999d
teixerya <78224021+teixerya@users.noreply.github.com>
2022-03-29 10:41:05 -0400

    Add files via upload

---
+++ assets/data/myCourses.ts
@@ -0,0 +1,9 @@
+import { Course } from "src/app/courseInfo";
+
+export const COURSE: Course[] = [
+    {ccode:'ABC', ctitle:'Angular L1', cday:'W', cprof:'Bart Simpson' },
+    {ccode:'BBC', ctitle:'Angular L2', cday:'T', cprof:'Homer Simpson' },
+    {ccode:'WUC', ctitle:'Python L1', cday:'F', cprof:'Stewie' },
+    {ccode:'DUC', ctitle:'Flutter L2', cday:'T', cprof:'Rick & Morty' }
+
+]
---
+++ assets/data/teixeryaData.ts
@@ -0,0 +1,10 @@
+import { Teixerya } from "src/app/teixerya";
+
+export const TEIXERYA: Teixerya = {
+    sid: 991,
+    sname: 'Ryan Teixeira',
+    slogin: 'teixerya',
+    semail: "teixerya@sheridancollege.ca",
+    scampus: "Trafalgar",
+    stitle: "Assignment_3"
+}
